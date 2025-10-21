6c2cae48d162c983107d17d9adb026c503d27258
tgubler9 <tgubler@iu.edu>
2025-10-21 17:02:37 -0400

    added test and refactored test code for ddb

--- src/critic_tests/test_libs/test_ddb.py
+++ src/critic_tests/test_libs/test_ddb.py
@@ -6,6 +6,75 @@ from critic.Monitor.MonitorIn import MonitorIn
 
 def test_it():
     # Create the table
+    client = create_monitor_table()
+
+    # Pretend we've received data via the API
+    API_DATA = {
+        'project_id': 'my-project',
+        'id': '6033aa47-a9f7-4d7f-b7ff-a11ba9b34474',
+        'url': 'https://example.com/health',
+        'interval': 5,
+        'next_due_at': 1234567890,  # epoch seconds
+        'timeout': 5,
+        'assertions': {'status_code': 200, 'body_contains': 'OK'},
+        'failures_before_alerting': 2,
+        'alert_slack_channels': ['#ops'],
+        'alert_emails': ['alerts@example.com'],
+        'realert_interval': 600,
+    }
+    # Double-check data is valid
+    MonitorIn(**API_DATA)
+
+    put_in_serialized(client, 'Monitor', API_DATA)
+    ddb_data = get_deserialized_data(client, 'my-project', API_DATA['id'])
+
+    assert ddb_data['id'] == '6033aa47-a9f7-4d7f-b7ff-a11ba9b34474'
+
+
+def test_two_inputs():
+    client = create_monitor_table()
+
+    API_DATA = {
+        'project_id': 'my-project',
+        'id': '6033aa47-a9f7-4d7f-b7ff-a11ba9b34474',
+        'url': 'https://example.com/health',
+        'interval': 5,
+        'next_due_at': 1234567890,  # epoch seconds
+        'timeout': 5,
+        'assertions': {'status_code': 200, 'body_contains': 'OK'},
+        'failures_before_alerting': 2,
+        'alert_slack_channels': ['#ops'],
+        'alert_emails': ['alerts@example.com'],
+        'realert_interval': 600,
+    }
+
+    API_DATA_2 = {
+        'project_id': 'my-project_2',
+        'id': '6033aa47-a9f7-4d7f-b7ff-a11ba9b34475',
+        'url': 'https://example.com/health',
+        'interval': 7,
+        'next_due_at': 1234567891,  # epoch seconds
+        'timeout': 3,
+        'assertions': {'status_code': 200, 'body_contains': 'OK'},
+        'failures_before_alerting': 3,
+        'alert_slack_channels': ['#ops_2'],
+        'alert_emails': ['alerts_2@example.com'],
+        'realert_interval': 1200,
+    }
+
+    MonitorIn(**API_DATA)
+    MonitorIn(**API_DATA_2)
+
+    put_in_serialized(client, 'Monitor', API_DATA)
+    put_in_serialized(client, 'Monitor', API_DATA_2)
+    
+    ddb_data_2 = get_deserialized_data(client, 'my-project_2', API_DATA_2['id'])
+   
+    assert ddb_data_2['id'] == '6033aa47-a9f7-4d7f-b7ff-a11ba9b34475'
+ 
+
+
+def create_monitor_table():
     client = boto3.client('dynamodb', region_name='us-east-2')
     client.create_table(
         TableName='Monitor',
@@ -34,30 +103,15 @@ def test_it():
         BillingMode='PAY_PER_REQUEST',
     )
 
-    # Pretend we've received data via the API
-    API_DATA = {
-        'project_id': 'my-project',
-        'id': '6033aa47-a9f7-4d7f-b7ff-a11ba9b34474',
-        'url': 'https://example.com/health',
-        'interval': 5,
-        'next_due_at': 1234567890,  # epoch seconds
-        'timeout': 5,
-        'assertions': {'status_code': 200, 'body_contains': 'OK'},
-        'failures_before_alerting': 2,
-        'alert_slack_channels': ['#ops'],
-        'alert_emails': ['alerts@example.com'],
-        'realert_interval': 600,
-    }
-    # Double-check data is valid
-    MonitorIn(**API_DATA)
+    return client
 
-    # Convert the data to DDB JSON format and store it
-    ddb_item = {k: serializer.serialize(v) for k, v in API_DATA.items()}
-    client.put_item(TableName='Monitor', Item=ddb_item)
-
-    # Retrieve the data and convert it back to a standard dict
+def get_deserialized_data(client, proj_id : str, data_id : str):
     ddb_item = client.get_item(
-        TableName='Monitor', Key={'project_id': {'S': 'my-project'}, 'id': {'S': API_DATA['id']}}
+        TableName='Monitor', Key={'project_id': {'S': proj_id}, 'id': {'S': data_id}}
     )['Item']
-    ddb_data = {k: deserializer.deserialize(v) for k, v in ddb_item.items()}
-    assert ddb_data['id'] == '6033aa47-a9f7-4d7f-b7ff-a11ba9b34474'
+
+    return {k: deserializer.deserialize(v) for k, v in ddb_item.items()}
+
+def put_in_serialized(client, table_name : str, API_content : dict):
+    ddb_item = {k: serializer.serialize(v) for k, v in API_content.items()}
+    client.put_item(TableName=table_name, Item=ddb_item)
