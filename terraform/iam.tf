7c4c4bb91d0448d07348b74ffb815420c8301dc1
Caleb Syring <caleb@syrings.us>
2025-12-08 18:40:03 -0500

    Add IUSDevs IAM role to test env

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -97,3 +97,75 @@ resource "aws_iam_policy" "lambda_dynamodb_test" {
     ]
   })
 }
+
+# Admin access group for IUS devs - test
+resource "aws_iam_group" "ius_devs_test" {
+  provider = aws.test
+  name     = "IUSDevs"
+}
+
+resource "aws_iam_policy" "mfa_self_manage" {
+  provider = aws.test
+  name     = "MFASelfManage"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowListActions",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:ListUsers",
+          "iam:ListVirtualMFADevices"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "AllowUserToCreateVirtualMFADevice",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:CreateVirtualMFADevice"
+        ],
+        "Resource" : "arn:aws:iam::*:mfa/*"
+      },
+      {
+        "Sid" : "AllowUserToManageTheirOwnMFA",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:EnableMFADevice",
+          "iam:GetMFADevice",
+          "iam:ListMFADevices",
+          "iam:ResyncMFADevice"
+        ],
+        "Resource" : "arn:aws:iam::*:user/$${aws:username}"
+      },
+      {
+        "Sid" : "AllowUserToDeactivateTheirOwnMFAOnlyWhenUsingMFA",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:DeactivateMFADevice"
+        ],
+        "Resource" : [
+          "arn:aws:iam::*:user/$${aws:username}"
+        ],
+        "Condition" : {
+          "Bool" : {
+            "aws:MultiFactorAuthPresent" : "true"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "ius_devs_admin_test" {
+  provider   = aws.test
+  group      = aws_iam_group.ius_devs_test.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "ius_devs_manage_mfa_test" {
+  provider   = aws.test
+  group      = aws_iam_group.ius_devs_test.name
+  policy_arn = aws_iam_policy.mfa_self_manage.arn
+}
