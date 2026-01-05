c178d39a02b6dfcb20f4081936d7c0c095c934f6
tgubler9 <tgubler@iu.edu>
2026-01-05 17:30:24 -0500

    test for paused monitor

--- tests/critic_tests/test_run_checks.py
+++ tests/critic_tests/test_run_checks.py
@@ -46,6 +46,11 @@ def assertions_pass(monitor: UptimeMonitor, repsonse: httpx.Response):
 
 # not sure where to put this for now
 def run_checks(monitor: UptimeMonitor, http_client : httpx.Client):
+    print("checking if paused")
+    print(monitor.state)
+    if monitor.state == MonitorState.paused:
+        print("returning because paused")
+        return
 
     start = time.perf_counter()
     try:
@@ -161,7 +166,7 @@ def test_run_checks(get_uptime_monitor):
     assert info['resp_code'] > 0
     assert info['latency_secs'] > 0
 
-def test_run_check_fail(get_uptime_monitor):
+def test_run_check_fail_with_consec_fails_above_threshold(get_uptime_monitor):
     monitor: UptimeMonitor = get_uptime_monitor
 
     mock_client = MagicMock()
@@ -207,3 +212,81 @@ def test_run_check_fail(get_uptime_monitor):
     assert info['status'] == MonitorState.down
     assert info['resp_code'] == 0
     assert info['latency_secs'] == -1
+
+def test_run_check_fail_with_consec_fails_below_threshold(get_uptime_monitor):
+    monitor: UptimeMonitor = get_uptime_monitor
+    monitor.consecutive_fails = 0
+    monitor.state = MonitorState.up
+
+    mock_client = MagicMock()
+    mock_client.head.side_effect = httpx.TimeoutException("Connection timed out")
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(namespace_table('UptimeMonitor'))
+    logs_table = dynamodb.Table(namespace_table('UptimeLog'))
+
+    table.put_item(
+        Item={
+            'project_id': monitor.project_id,
+            'slug': monitor.slug,
+            'GSI_PK': 'MONITOR',
+            'state': monitor.state,
+            'url': monitor.url,
+            'consecutive_fails': monitor.consecutive_fails,
+            'next_due_at': monitor.next_due_at,
+            'timeout_secs': Decimal(str(monitor.timeout_secs)),
+            'failures_before_alerting': monitor.failures_before_alerting,
+            'realert_interval_mins': monitor.realert_interval_mins,
+        }
+    )
+
+    time_to_check = monitor.next_due_at
+    run_checks(monitor, http_client=mock_client)
+
+    response = table.get_item(Key={'project_id': monitor.project_id, 'slug': monitor.slug})
+    info = response['Item']
+    assert info['state'] == MonitorState.up
+    assert info['consecutive_fails'] == 1
+
+    monitor_id = monitor.project_id + monitor.slug
+    response = logs_table.get_item(Key={'monitor_id': monitor_id, 'timestamp': time_to_check})
+    info = response['Item']
+    #log should have resp of 0 since there was a timeout, and a latency of -1
+    assert info['status'] == MonitorState.up
+    assert info['resp_code'] == 0
+    assert info['latency_secs'] == -1
+
+#what are we doing for next due time when paused?
+def test_run_check_fail_with_paused_monitor(get_uptime_monitor):
+    monitor: UptimeMonitor = get_uptime_monitor
+    monitor.state = MonitorState.paused
+    
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(namespace_table('UptimeMonitor'))
+    logs_table = dynamodb.Table(namespace_table('UptimeLog'))
+
+    table.put_item(
+        Item={
+            'project_id': monitor.project_id,
+            'slug': monitor.slug,
+            'GSI_PK': 'MONITOR',
+            'state': monitor.state,
+            'url': monitor.url,
+            'consecutive_fails': monitor.consecutive_fails,
+            'next_due_at': monitor.next_due_at,
+            'timeout_secs': Decimal(str(monitor.timeout_secs)),
+            'failures_before_alerting': monitor.failures_before_alerting,
+            'realert_interval_mins': monitor.realert_interval_mins,
+        }
+    )
+
+    time_to_check = monitor.next_due_at
+    client : httpx.Client = httpx.Client()
+    run_checks(monitor, client)
+    client.close()
+
+    monitor_id = monitor.project_id + monitor.slug
+    response = logs_table.get_item(Key={'monitor_id': monitor_id, 'timestamp': time_to_check})
+    #does not have item because no log is created since the monitor is paused
+    assert 'Item' not in response
