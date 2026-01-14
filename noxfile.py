7925b5e507359a8cff58d507150fdd6108b78da1
tgubler9 <tgubler@iu.edu>
2026-01-14 18:08:23 -0500

    modifed nox again

--- noxfile.py
+++ noxfile.py
@@ -13,8 +13,8 @@ nox.options.default_venv_backend = 'uv'
 @nox.session
 def pytest(session: nox.Session):
     uv_sync(session)
-    pytest_run(session, '-m', 'not integration')
     pytest_run(session, '-m', 'integration')
+    pytest_run(session, '-m', 'not integration')
 
 
 @nox.session
